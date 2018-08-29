aecdbd2ed965b0d320ee7de197b800bb3ef4372f
Anibal <anibal.siguenza1@gmail.com>
2018-08-29 16:26:04 +0200

    Add tmuxThesis initialization

---
+++ tmuxThesis.sh
@@ -0,0 +1,19 @@
+#Actions first window
+tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/ALPACA-Hoppe' Enter
+tmux send-keys -t 1 'vim' Enter
+tmux send-keys -t 1 ':NERDTreeToggle' Enter
+
+#Actions second window
+tmux new-window
+tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/ALPACA-Hoppe' Enter
+
+#Actions third window
+tmux new-window
+tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/dummy' Enter
+tmux send-keys -t 1 'vim' Enter
+tmux send-keys -t 1 ':NERDTreeToggle' Enter
+tmux split-window
+tmux select-pane
+
+#After creation actions
+tmux select-window -t 1
