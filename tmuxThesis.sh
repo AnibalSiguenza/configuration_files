724059d11527683bb0410a2fb705567b747bba21
Anibal <anibal.siguenza1@gmail.com>
2018-10-15 17:55:27 +0200

    Open main and input on tabs

--- tmuxThesis.sh
+++ tmuxThesis.sh
@@ -2,6 +2,10 @@
 tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/ALPACA-Hoppe' Enter
 tmux send-keys -t 1 'vim' Enter
 tmux send-keys -t 1 ':NERDTreeToggle' Enter
+tmux send-keys -t 1 ':tabnew src/main.cpp' Enter
+tmux send-keys -t 1 ':tabnew inputfile.xml' Enter
+tmux send-keys -t 1 'gt' Enter
+tmux send-keys -t 1 '5j' Enter
 
 #Actions second window
 tmux new-window
