a2d8142e42689aee176e7e0bb7050cedf5ed5c46
Anibal <anibal.siguenza1@gmail.com>
2019-02-19 10:51:46 +0100

    update thesis tmux stuff

--- thesisTex.sh
+++ thesisTex.sh
@@ -8,6 +8,9 @@ tmux send-keys -t 1 '3j' Enter
 tmux new-window
 tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/thesis_tex' Enter
 tmux send-keys -t 1 'xdg-open build/main.pdf' Enter
+tmux send-keys -t 1 'google-chrome https://docs.google.com/document/d/14O23DCX6maGg8eHe3tJULXQ6OWZTE3n1EEWJK7vzito/edit &' Enter
+tmux send-keys -t 1 'google-chrome https://translate.google.com/ &' Enter
+tmux send-keys -t 1 'google-chrome https://translate.google.com/ &' Enter
 
 #After creation actions
 tmux select-window -t 1
--- tmuxThesis.sh
+++ tmuxThesis.sh
@@ -2,22 +2,11 @@
 tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/ALPACA-Hoppe' Enter
 tmux send-keys -t 1 'vim' Enter
 tmux send-keys -t 1 ':NERDTreeToggle' Enter
-tmux send-keys -t 1 ':tabnew src/main.cpp' Enter
-tmux send-keys -t 1 ':tabnew inputfile.xml' Enter
-tmux send-keys -t 1 'gt' Enter
 tmux send-keys -t 1 '3j' Enter
 
 #Actions second window
 tmux new-window
 tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/ALPACA-Hoppe' Enter
-
-#Actions third window
-tmux new-window
-tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/dummy' Enter
-tmux send-keys -t 1 'vim' Enter
-tmux send-keys -t 1 ':NERDTreeToggle' Enter
-tmux split-window
-tmux send-keys -t 2 'cd ~/Documents/TUM/thesis/dummy' Enter
 
 #After creation actions
 tmux select-window -t 1
