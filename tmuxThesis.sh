0ee059264a5b5258e58ac0fe65b5fbce3238575c
Anibal <anibal.siguenza1@gmail.com>
2019-03-04 10:34:19 +0100

    change dir name

--- tmuxThesis.sh
+++ tmuxThesis.sh
@@ -1,12 +1,12 @@
 #Actions first window
-tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/ALPACA-Hoppe' Enter
+tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/ALPACA' Enter
 tmux send-keys -t 1 'vim' Enter
 tmux send-keys -t 1 ':NERDTreeToggle' Enter
 tmux send-keys -t 1 '3j' Enter
 
 #Actions second window
 tmux new-window
-tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/ALPACA-Hoppe' Enter
+tmux send-keys -t 1 'cd ~/Documents/TUM/thesis/ALPACA' Enter
 
 #After creation actions
 tmux select-window -t 1
